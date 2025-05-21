db3e3e1f82f2f57e0b47cef793e2d3c70354ade1
KapilSareen <kapilsareen584@gmail.com>
2025-05-22 03:07:21 +0530

    minor progress
    Signed-off-by: KapilSareen <kapilsareen584@gmail.com>

--- frontend/src/lib/components/gadget-attribs/extrainfo.svelte
+++ frontend/src/lib/components/gadget-attribs/extrainfo.svelte
@@ -1,13 +1,26 @@
 <script>
-	import { getContext } from 'svelte';
 	import Monaco from '../monaco.svelte';
+	import { getContext } from 'svelte';
+	import { page } from '$app/state';
+	import { environments } from '$lib/shared/environments.svelte.js';
 
-	let { gadgetInfo } = $props();
-    
+	let { data } = $props();
+
+	const api = getContext('api');
+	let env = $derived(environments[page.params.env]);
 	let gadgetInfoSource = $state('');
+
 	$effect(() => {
-		console.log('new src');
-		gadgetInfoSource = JSON.stringify(gadgetInfo.ExtraInfo, null, '  ');
+		if (!env.id || !data?.url) {
+			gadgetInfoSource = "";
+			return;
+		}
+		api.request({ cmd: 'getGadgetInfo', data: { url: data.url, environmentID: env.id } }).then((res) => {
+			gadgetInfoSource = JSON.stringify(res, null, '  ');
+		}).catch((err) => {
+            console.error('Error fetching gadget info:', err);
+            gadgetInfoSource = "";
+        });
 	});
 </script>
 
