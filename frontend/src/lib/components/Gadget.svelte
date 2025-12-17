fa909534bab2106890800dd3f719a9c372f8bd6f
Michael Friese <mfriese@microsoft.com>
2025-12-17 19:04:42 +0100

    enh: add cancel button for search input
    Signed-off-by: Michael Friese <mfriese@microsoft.com>

--- frontend/src/lib/components/Gadget.svelte
+++ frontend/src/lib/components/Gadget.svelte
@@ -338,13 +338,30 @@
 							{elapsedTime}
 						</div>{/if}
 					<div class="flex items-center px-2">
-						<Input
-							bind:this={searchInputRef}
-							bind:value={searchInput}
-							placeholder="Search..."
-							class="text-sm rounded-r-none"
-							onkeydown={handleSearchKeydown}
-						/>
+						<div class="relative">
+							<Input
+								bind:this={searchInputRef}
+								bind:value={searchInput}
+								placeholder="Search..."
+								class="text-sm rounded-r-none pr-8"
+								onkeydown={handleSearchKeydown}
+							/>
+							{#if searchInput}
+								<button
+									onclick={() => {
+										searchInput = '';
+										searchInputRef?.focus();
+									}}
+									class="absolute right-2 top-1/2 -translate-y-1/2 flex h-5 w-5 items-center justify-center rounded-full text-gray-400 hover:text-gray-600 dark:hover:text-gray-200 hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors"
+									title="Clear search"
+								>
+									<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+										<line x1="18" y1="6" x2="6" y2="18"></line>
+										<line x1="6" y1="6" x2="18" y2="18"></line>
+									</svg>
+								</button>
+							{/if}
+						</div>
 						<button
 							onclick={toggleSearchMode}
 							ondblclick={openSearchModeSettings}
